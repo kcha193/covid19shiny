52c83e8aa97d9c2c819e8454188780b1312fc228
Kevin Chang <kevin.ct.chang@gmail.com>
2021-01-06 20:41:14 +1300

    Fixed the US daily data by using the specific US data, not gobal data

--- R/get_daily_data.R
+++ R/get_daily_data.R
@@ -6,11 +6,15 @@ library(tidyverse)
 
 req <- httr::GET("https://api.github.com/repos/CSSEGISandData/COVID-19/git/trees/master?recursive=1")
 httr::stop_for_status(req)
-filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
+filelist_raw <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
+
+filelist_us <- 
+  grep("csse_covid_19_data/csse_covid_19_daily_reports_us/([0-9]+)",
+     filelist_raw, value = TRUE)
 
 filelist <- 
   grep("csse_covid_19_data/csse_covid_19_daily_reports/([0-9]+)",
-     filelist, value = TRUE)
+     filelist_raw, value = TRUE)
 
 dates <-
   lubridate::mdy(gsub(".csv$", "", basename(filelist)))
@@ -29,9 +33,27 @@ latest_daily_cases_total <-
   latest_daily_cases %>%
   select(Country_Region, Confirmed:Active) %>%
   rename(Country = Country_Region) %>%
+  filter(Country != "US") %>% 
   pivot_longer(Confirmed:Active, names_to = "Type", values_to = "Count") %>%
   group_by(Country, Type) %>%
   summarise(Count_lastest = sum(Count, na.rm = TRUE), .groups = "drop")
+
+latest_daily_cases_us <-
+  read_csv(
+    paste0(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/",
+      filelist_us[which(dates == max(dates, na.rm = TRUE))]
+    )
+  )
+
+latest_daily_cases_total <-
+  latest_daily_cases_us %>%
+  select(Country_Region, Confirmed:Active) %>%
+  rename(Country = Country_Region) %>%
+  pivot_longer(Confirmed:Active, names_to = "Type", values_to = "Count") %>%
+  group_by(Country, Type) %>%
+  summarise(Count_lastest = sum(Count, na.rm = TRUE), .groups = "drop") %>% 
+  bind_rows(latest_daily_cases_total)
 
 
 previous_daily_cases <-
@@ -46,9 +68,28 @@ previous_daily_cases_total <-
   previous_daily_cases %>%
   select(Country_Region, Confirmed:Active) %>%
   rename(Country = Country_Region) %>%
+  filter(Country != "US") %>% 
   pivot_longer(Confirmed:Active, names_to = "Type", values_to = "Count") %>%
   group_by(Country, Type) %>%
   summarise(Count_previous = sum(Count, na.rm = TRUE), .groups = "drop")
+
+
+previous_daily_cases_us <-
+  read_csv(
+    paste0(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/",
+      filelist_us[which(dates == sort(dates, decreasing = TRUE)[2])]
+    )
+  )
+
+previous_daily_cases_total <-
+  previous_daily_cases_us %>%
+  select(Country_Region, Confirmed:Active) %>%
+  rename(Country = Country_Region) %>%
+  pivot_longer(Confirmed:Active, names_to = "Type", values_to = "Count") %>%
+  group_by(Country, Type) %>%
+  summarise(Count_previous = sum(Count, na.rm = TRUE), .groups = "drop")%>% 
+  bind_rows(previous_daily_cases_total)
 
 
 daily_cases_total <- 
